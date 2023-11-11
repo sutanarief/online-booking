e85f95744befc088f1528ea154802f1eb1cb7a91
sutanarief <sutan1412@gmail.com>
2023-11-12 04:36:47 +0700

    update port

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const router = require('./routes/index')
 const routes = require('./routes/index')
 const cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1', routes)
 
